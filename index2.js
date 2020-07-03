c5731662d1fd427e29e319e7b9ff967d56249071
Caroline <carolinejepps@gmail.com>
2020-07-03 06:16:08 +0000

    Added Promise versions of files

---
+++ index2.js
@@ -0,0 +1,14 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+// .then((coords) => console.log(coords))
